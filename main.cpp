16f5dcd52ce132791e5a386dcf733681d2e27640
agne burokaite <agbu4307@kiss.MIF>
2018-02-20 10:21:52 +0200

    trecias

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <string>
-#include <random>
 #include <iomanip>
+#include <fstream>
+#include <vector>
+
+
 int main()
+
 {
-    std::string var, pav;
-    int n;
-    int egz;
-    float total=0;
-    std::mt19937 mt();
-    std::uniform_int_distribution d(1,10);
-    std::cout<<"iveskit varda, tada enter, tada pavarde"<<std::endl;
-    std::cin>>var>>pav;
-    n=d(mt);
-    egz=d(mt);
-    for(int i=0;i<n;i++)
+    float total;
+    std::ifstream df ("duom.txt");
+    char b;
+    std::vector<char> v[5];
+    while(b!='\n')
     {
-        total=total+d(mt);
-        std::cout<<total<<"\n";
+        b=df.get();
     }
-    total=total/n*0.4+egz*0.6;
-    std::cout << std::setprecision(3) << total << '\n';
     return 0;
 }
