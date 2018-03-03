8f58eb08f5f1efb96baffba5414f6ec1f970d3f0
agne burokaite <agne.burokaite@gmail.com>
2018-03-03 16:08:18 +0200

    V0.4 laiko skaiciavimas. gal

--- main.cpp
+++ main.cpp
@@ -1,180 +1,187 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <random>
 #include <iomanip>
-#include <vector>
-#include "studentai.h"
-#include "sukeist.h"
-
+#include <chrono>
+const int n=20;
+typedef std::chrono::high_resolution_clock Clock;
 int main()
+
 {
-    std::ifstream df("kursiokai.txt");
-    if(!df)
-    {
-        std::cout<<"failas neegzistuoja";
-        return 0;
-    }
-    char b;
-    std::size_t n;
-    std::vector<studentai> a;
-    a.resize(1);
-    while(b!='\n')
-    {
-        df.get(b);
-    }
-    df.get(b);
-    std::size_t j=0;
-    while (isalpha(b)>0)
-    {
-        while(b!='\t')
-        {
-            a[j].var.push_back(b);
-            df.get(b);
-        }
-        df.get(b);
-        if(b=='\t')
-        {
-            df.get(b);
-        }
-        while(b!='\t')
-        {
-            a[j].pav.push_back(b);
-            df.get(b);
-        }
-        try
-        {
-            while (b!='\n')
-            {
-                df>>n;
-                a[j].paz.push_back(n);
-                df.get(b);
-            }
-        }
-        catch (std::exception& e)
-        {
-            std::cout<<"kursiokai.txt failas neteisingu formatu. greaiciausiai nera paciam gale perejimo i kita eilute.\n";
-            return 0;
-        }
-        a[j].egz=a[j].paz.back();
-        a[j].paz.pop_back();
-        df.get(b);
-        j++;
-        a.resize(j+1);
-    }
-    df.close();
-    a.resize(j);
-    try
-    {
-        for(int i=0; i<j-1; i++)
-        {
-            if(a[i].pav>a[i+1].pav)
-            {
-                sukeist(a[i],a[i+1]);
-                i=-1;
-            }
-        }
-        for(std::size_t i=0; i<j; i++) //suskaiciuoja nd vidurkius/medianas
-        {
 
-            n=a[i].paz.size();
-            a[i].vidnd=0;
-            for(std::size_t o=0; o<n; o++)
-            {
-                a[i].vidnd=a[i].vidnd+a[i].paz[o];
-            }
-            a[i].vidnd=a[i].vidnd/n;
-            for(int o=0; o<n-1; o++)        //isrikiuoju nuo did iki maz
-            {
-                if(a[i].paz[o]<a[i].paz[o+1])
-                {
-                    a[i].paz[o]=a[i].paz[o]+a[i].paz[o+1];
-                    a[i].paz[o+1]=a[i].paz[o]-a[i].paz[o+1];
-                    a[i].paz[o]=a[i].paz[o]-a[i].paz[o+1];
-                    o=-1;
-                }
-            }
-            if(n%2==0)
-            {
-                a[i].mednd=(a[i].paz[(n/2)-1]+a[i].paz[n/2])/2;
-            }
-            else
-            {
-                a[i].mednd=a[i].paz[n/2];
-            }
 
-        }
-        std::size_t f=j; //f zymes kelintam elemente jau yra nelaike egzamino vaikai
-        for(int i=0; i<j; i++)
-        {
-            if((a[i].vidnd<6 || a[i].mednd<6) && i<f)
-            {
-                for(int o=i; o<j-1; o++)
-                {
-                    sukeist(a[o],a[o+1]);
-                }
-                i--;
-                f--;
-            }
-        }
-        std::cout<<"The cool kidz:\n";
-        std::cout<<"Vardas\t\tPavarde\t\tGal. vidurkis\tGal. mediana\n";
-        for(std::size_t i=0; i<f; i++)
-        {
-            std::cout<<a[i].var;
-            if (a[i].var.size()<8)
-            {
-                std::cout<<"\t\t";
-            }
-            else
-            {
-                std::cout<<"\t";
-            }
-            std::cout<<a[i].pav;
-            if (a[i].pav.size()<8)
-            {
-                std::cout<<"\t\t";
-            }
-            else
-            {
-                std::cout<<"\t";
-            }
-
-            a[i].vidtotal=a[i].vidnd*0.4+a[i].egz*0.6;
-            a[i].medtotal=a[i].mednd*0.4+a[i].egz*0.6;
-            std::cout<<std::setprecision(3)<<a[i].vidtotal<<"\t\t";
-            std::cout<<std::setprecision(3)<<a[i].medtotal<<"\n";
-
-        }
-        if(f<j){
-        std::cout<<"Da stoopid loosers:\n";
-        std::cout<<"Vardas\t\tPavarde\t\tnd vidurkis\tnd mediana\n";
-        for(f; f<j; f++)
-        {
-            std::cout<<a[f].var;
-            if (a[f].var.size()<8)
-            {
-                std::cout<<"\t\t";
-            }
-            else
-            {
-                std::cout<<"\t";
-            }
-            std::cout<<a[f].pav;
-            if (a[f].pav.size()<8)
-            {
-                std::cout<<"\t\t";
-            }
-            else
-            {
-                std::cout<<"\t";
-            }
-
-            std::cout<<std::setprecision(3)<<a[f].vidnd<<"\t\t"<<a[f].mednd<<"\n";
-        }}
-    }
-    catch (std::bad_alloc& e)
+    std::string var, pav;
+    int egz;
+    float total;
+    int paz;
+    std::random_device rd; //lel ant mano kompiuterio irgi neveikia, ta pati skaiciu generuoja
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<> d(1,10);
+    auto t1=Clock::now();
+    //pirmas failas
+    std::ofstream rf("1.txt");
+    rf<<"vardas\t\tpavarde\t\t";
+    for(int i=1;i<=n;i++)
     {
-        std::cout<<"negalime isrikiuot vardu.";
+        rf<<"nd"<<i<<"\t";
     }
+    rf<<"egzaminas\tgalutinis balas\n";
+    for(int j=0;j<10;j++)
+    {
+    pav="pavarde"+std::to_string(j+1);
+    var="vardas"+std::to_string(j+1);
+    rf<<var;
+    if(var.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    rf<<pav;
+    if(pav.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    egz=d(mt);
+    total=0;
+    for(int i=0; i<n; i++)
+    {
+        paz=d(mt);
+        rf<<paz<<"\t";
+        total=total+paz;
+    }
+
+    rf<<egz<<"\t\t";
+    total=total/n*0.4+egz*0.6;
+    rf << std::setprecision(3) << total << '\n';
+    }
+    rf.close();
+    auto t2 = Clock::now();
+    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
+    t1=Clock::now();
+
+    //antras failas
+    rf.open("2.txt");
+    rf<<"vardas\t\tpavarde\t\t";
+    for(int i=1;i<=n;i++)
+    {
+        rf<<"nd"<<i<<"\t";
+    }
+    rf<<"egzaminas\tgalutinis balas\n";
+    for(int j=0;j<100;j++)
+    {
+    pav="pavarde"+std::to_string(j+1);
+    var="vardas"+std::to_string(j+1);
+    rf<<var;
+    if(var.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    rf<<pav;
+    if(pav.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    egz=d(mt);
+    total=0;
+    for(int i=0; i<n; i++)
+    {
+        paz=d(mt);
+        rf<<paz<<"\t";
+        total=total+paz;
+    }
+
+    rf<<egz<<"\t\t";
+    total=total/n*0.4+egz*0.6;
+    rf << std::setprecision(3) << total << '\n';
+    }
+    rf.close();
+    t2 = Clock::now();
+    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
+    t1 = Clock::now();
+    //trecias failas
+    rf.open("3.txt");
+    rf<<"vardas\t\tpavarde\t\t";
+    for(int i=1;i<=n;i++)
+    {
+        rf<<"nd"<<i<<"\t";
+    }
+    rf<<"egzaminas\tgalutinis balas\n";
+    for(int j=0;j<1000;j++)
+    {
+    pav="pavarde"+std::to_string(j+1);
+    var="vardas"+std::to_string(j+1);
+    rf<<var;
+    if(var.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    rf<<pav;
+    if(pav.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    egz=d(mt);
+    total=0;
+    for(int i=0; i<n; i++)
+    {
+        paz=d(mt);
+        rf<<paz<<"\t";
+        total=total+paz;
+    }
+
+    rf<<egz<<"\t\t";
+    total=total/n*0.4+egz*0.6;
+    rf << std::setprecision(3) << total << '\n';
+    }
+    rf.close();
+    t2 = Clock::now();
+    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
+    t1 = Clock::now();
+    //ketvirtas failas
+    rf.open("4.txt");
+    rf<<"vardas\t\tpavarde\t\t";
+    for(int i=1;i<=n;i++)
+    {
+        rf<<"nd"<<i<<"\t";
+    }
+    rf<<"egzaminas\tgalutinis balas\n";
+    for(int j=0;j<10000;j++)
+    {
+    pav="pavarde"+std::to_string(j+1);
+    var="vardas"+std::to_string(j+1);
+    rf<<var;
+    if(var.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    rf<<pav;
+    if(pav.length()>=8)
+    {
+        rf<<"\t";
+    }
+    else rf<<"\t\t";
+    egz=d(mt);
+    total=0;
+    for(int i=0; i<n; i++)
+    {
+        paz=d(mt);
+        rf<<paz<<"\t";
+        total=total+paz;
+    }
+
+    rf<<egz<<"\t\t";
+    total=total/n*0.4+egz*0.6;
+    rf << std::setprecision(3) << total << '\n';
+    }
+    rf.close();
+    t2 = Clock::now();
+    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
     return 0;
 }
