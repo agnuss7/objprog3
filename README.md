8874759b5918a785c3c5d64f84ab4dba8f5d0e98
agne burokaite <agne.burokaite@gmail.com>
2018-03-08 12:06:36 +0200

    V0.4 laiko skaiciavimas(baigtas?)

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿## V0.4 laiko skaičiavimas
+# Trukmių lentelė
+Generuotų studentų skaičius faile | Trukmė (nanosekundėmis)
+------------ | -------------
+10 | 3051000
+100 | 7520000
+1000 | 43140000
+10000 | 176969000
+100000 | 1765720000
--- main.cpp
+++ main.cpp
@@ -6,19 +6,21 @@
 #include <chrono>
 const int n=20;
 typedef std::chrono::high_resolution_clock Clock;
-
+typedef std::chrono::nanoseconds TimeType;
 int main()
 
 {
-
     std::string var, pav;
     int egz;
     float total;
     int paz;
-    //std::random_device rd;
-    std::mt19937 mt (5468);//mt(rd());
+    std::random_device rd;
+    std::mt19937 mt(rd());
     std::uniform_int_distribution<> d(1,10);
-    auto t1=Clock::now();
+
+
+
+    std::chrono::high_resolution_clock::time_point t1=Clock::now();
     //pirmas failas
     std::ofstream rf("1.txt");
     rf<<"vardas\t\tpavarde\t\t";
@@ -57,8 +59,8 @@ int main()
     rf << std::setprecision(3) << total << '\n';
     }
     rf.close();
-    auto t2 = Clock::now();
-    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
+    std::chrono::high_resolution_clock::time_point t2 = Clock::now();
+    std::cout<<(std::chrono::duration_cast<TimeType>(t2 - t1)).count()<<"\n";
     t1=Clock::now();
 
     //antras failas
@@ -100,8 +102,7 @@ int main()
     }
     rf.close();
     t2 = Clock::now();
-    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
-
+    std::cout<<std::chrono::duration_cast<TimeType>(t2 - t1).count()<<"\n";
     t1 = Clock::now();
     //trecias failas
     rf.open("3.txt");
@@ -142,7 +143,7 @@ int main()
     }
     rf.close();
     t2 = Clock::now();
-    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
+    std::cout<<std::chrono::duration_cast<TimeType>(t2 - t1).count()<<"\n";
     t1 = Clock::now();
     //ketvirtas failas
     rf.open("4.txt");
@@ -183,8 +184,7 @@ int main()
     }
     rf.close();
     t2 = Clock::now();
-    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
-
+    std::cout<<std::chrono::duration_cast<TimeType>(t2 - t1).count()<<"\n";
     t1=Clock::now();
 
     //penktas failas
@@ -226,7 +226,6 @@ int main()
     }
     rf.close();
     t2 = Clock::now();
-    std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<"\n";
-
+    std::cout<<std::chrono::duration_cast<TimeType>(t2 - t1).count()<<"\n";
     return 0;
 }
