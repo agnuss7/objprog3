8ce8454757c6f4419ee9a8aee019a567e274ce28
agne burokaite <agne.burokaite@gmail.com>
2018-06-18 12:56:58 +0300

    V2.0

--- doxygen/html/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vidurkis',['vidurkis',['../classstudentai.html#a77b16e0d365f9e3d875979bec7852d8b',1,'studentai']]]
-];
--- doxygen/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['v0_2e1',['V0.1',['../md__c_1__users__agne__desktop_mokslai_objprog3__r_e_a_d_m_e.html',1,'']]]
-];
